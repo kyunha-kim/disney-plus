c8e92e807d0b608c964fe1767cf9ed373f1a6ae6
kim-seonkyu <seonkyu93@gmail.com>
2023-02-27 18:22:39 +0900

    react-router-dom 적용

---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import requests from "../../api/request";
+import Banner from "../../components/Banner";
+import Category from "../../components/Category";
+import Nav from "../../components/Nav";
+import Row from "../../components/Row";
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after {
+    content: "";
+    background-image: url("/images/home-background.png");
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    position: absolute;
+    inset: 0;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
+
+const MainPage = () => {
+  return (
+    <Container>
+      <Nav />
+      <Banner />
+      <Category />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+    </Container>
+  );
+};
+
+export default MainPage;
