61d5f4fc4cea4303e7bc20b77ee748bab458aeea
kim-seonkyu <seonkyu93@gmail.com>
2023-02-28 01:27:14 +0900

    fixed http -> https

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "3717b6c632b8434e14bd5345d2d24397",
     language: "ko-KR",
